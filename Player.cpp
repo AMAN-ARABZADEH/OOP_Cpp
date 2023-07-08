1aac788931709cca81e67c8edf1168254404f6d7
Aman Arabzadeh <aman.arabzadeh98@gmail.com>
2023-07-09 01:15:53 +0200

    OOP - Object-Oriented Programming

--- Player.cpp
+++ Player.cpp
@@ -1,38 +1,51 @@
 #include "Player.h"
 
 // Constructors
-Player::Player() : name(""), level(1), health(100) {}
 
+// Default constructor
+Player::Player() : name(""), level(0), health(0) {}
+
+// Parameterized constructor
 Player::Player(const std::string& name, int level, int health)
         : name(name), level(level), health(health) {}
 
 // Getters
+
+// Get the player's name
 std::string Player::getName() const {
     return name;
 }
 
+// Get the player's level
 int Player::getLevel() const {
     return level;
 }
 
+// Get the player's health
 int Player::getHealth() const {
     return health;
 }
 
 // Setters
+
+// Set the player's name
 void Player::setName(const std::string& newName) {
     name = newName;
 }
 
+// Set the player's level
 void Player::setLevel(int newLevel) {
     level = newLevel;
 }
 
+// Set the player's health
 void Player::setHealth(int newHealth) {
     health = newHealth;
 }
 
 // Member functions
+
+// Reduce the player's health by the specified damage amount
 void Player::takeDamage(int damageAmount) {
     health -= damageAmount;
     if (health < 0) {
@@ -40,15 +53,14 @@ void Player::takeDamage(int damageAmount) {
     }
 }
 
+// Increase the player's level by 1
 void Player::levelUp() {
     level++;
-    health = 100;
 }
 
 // Overloading the << operator for Player class
 std::ostream& operator<<(std::ostream& os, const Player& player) {
-    os << "Player Name: " << player.getName() << std::endl;
-    os << "Player Level: " << player.getLevel() << std::endl;
-    os << "Player Health: " << player.getHealth() << std::endl;
+    os << "Player Name: " << player.getName() << ", Level: " << player.getLevel()
+       << ", Health: " << player.getHealth();
     return os;
 }
--- Player.h
+++ Player.h
@@ -4,6 +4,7 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+
 #include <iostream>
 #include <string>
 
